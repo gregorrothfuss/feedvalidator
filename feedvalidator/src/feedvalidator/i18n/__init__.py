403735109a4d09c2c74d1507218ac36ac785ea74
Joe Gregorio <joe@bitworking.org>
2007-05-07 20:56:03 +0000

    Branching for a large refactoring and moving GUI specific stuff out and improving caching.

--- feedvalidator/src/feedvalidator/i18n/__init__.py
+++
@@ -1,6 +0,0 @@
-"""$Id$"""
-
-__author__ = "Sam Ruby <http://intertwingly.net/> and Mark Pilgrim <http://diveintomark.org/>"
-__version__ = "$Revision$"
-__date__ = "$Date$"
-__copyright__ = "Copyright (c) 2002 Sam Ruby and Mark Pilgrim"
